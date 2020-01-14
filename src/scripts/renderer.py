ba92523d97784602deb30ca65c54f34a84ea6e84
Parika Goel <parikagoel@gmail.com>
2020-01-14 10:07:59 +0100

    exception handling

--- src/scripts/renderer.py
+++ src/scripts/renderer.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import shutil
 import pathlib
 import trimesh
 import pyrender
@@ -128,16 +129,49 @@ def generate_images(obj_file, out_folder, num_renderings):
         count += 1
 
 
+# if __name__ == '__main__':
+#     num_renderings = 10
+#
+#     params = JSONHelper.read("./parameters.json")
+#
+#     for f in glob.glob(params["shapenet"] + "/**/*/models/model_normalized.obj"):
+#         catid_cad = f.split("/", 6)[4]
+#         id_cad = f.split("/", 6)[5]
+#
+#         print("catid: ", catid_cad, " , id: ", id_cad)
+#         outdir = params["shapenet_renderings"] + "/" + catid_cad + "/" + id_cad
+#         pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+#
+#         generate_images(f, outdir, num_renderings)
+
 if __name__ == '__main__':
     num_renderings = 10
 
     params = JSONHelper.read("./parameters.json")
 
-    for f in glob.glob(params["shapenet"] + "/**/*/models/model_normalized.obj"):
-        catid_cad = f.split("/", 6)[4]
+    failed_cases = {}
+    failed_ids = []
+    file = "./failed_cases.json"
+
+    catid_cad = "03211117"
+    # id_cad = "adf9b0eaf3a1980187710597b0363fe6"
+
+    for f in glob.glob(params["shapenet"] + catid_cad + "/*/models/model_normalized.obj"):
         id_cad = f.split("/", 6)[5]
+        print("catid_cad: ", catid_cad, " , id_cad: ", id_cad)
+        try :
+            obj_file = params["shapenet"] + "/" + catid_cad + "/" + id_cad + "/models/model_normalized.obj"
+            outdir = params["shapenet_renderings"] + "/" + catid_cad + "/" + id_cad
+            pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
 
-        outdir = params["shapenet_renderings"] + "/" + catid_cad + "/" + id_cad
-        pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+            generate_images(obj_file, outdir, num_renderings)
 
-        generate_images(f, outdir, num_renderings)
+            dir = params["shapenet"] + "/" + catid_cad + "/" + id_cad
+            shutil.rmtree(dir)
+        except:
+            failed_ids.append(id_cad)
+            pass
+
+    failed_cases[catid_cad] = failed_ids
+    write(file, failed_cases)
+
--- src/scripts/test.py
+++ src/scripts/test.py
@@ -1,13 +1,7 @@
 import numpy as np
 from PIL import Image
+import shutil
 
 if __name__ == '__main__':
-    depth_file = "/home/parika/WorkingDir/complete3D/Assets/shapenet-renderings/02747177/85d8a1ad55fa646878725384d6baf445/depth/depth0.png"
-    color_file = "/home/parika/WorkingDir/complete3D/Assets/shapenet-renderings/02747177/85d8a1ad55fa646878725384d6baf445/color/color0.png"
-
-    im = Image.open(color_file)
-    np_img = np.array(im)
-
-    depth_im = Image.open(depth_file)
-    d_np_img = np.array(depth_im)
-    print(d_np_img.shape)
+    dir = "/home/parika/WorkingDir/complete3D/Assets/testing"
+    shutil.rmtree(dir)
